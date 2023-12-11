6fe4b1cead0e017f402938108ab483959848d88d
Amr006 <amr.khaled.mohamed.mm@gmail.com>
2023-12-11 12:35:08 +0200

    adding facet aggregation

--- Project1/queries.js
+++ Project1/queries.js
@@ -46,3 +46,52 @@ and get the number the total user following events
 /////////////////////////////////////////////////////////////////////////////////////////
 
 
+const result = await Venue.aggregate([
+  {
+    $facet: {
+      featured: [
+        {
+          $match: {
+            Featured: true,
+          },
+        },
+        {
+          $project: {
+            Latitude: 0,
+            Longitude: 0,
+            UpcomingEvent: 0,
+            Followers: 0,
+            Meeting_space: 0,
+            FlagedEvents: 0,
+            OverviewImages: 0,
+            Images: 0,
+          },
+        },
+      ],
+      notFeatured: [
+        {
+          $match: {
+            Featured: false,
+          },
+        },
+        {
+          $project: {
+            Latitude: 0,
+            Longitude: 0,
+            UpcomingEvent: 0,
+            Followers: 0,
+            Meeting_space: 0,
+            FlagedEvents: 0,
+            OverviewImages: 0,
+            Images: 0,
+          },
+        },
+      ],
+    },
+    
+  },
+  {
+    $limit : parseInt(req.query.limit) ,
+  },
+]);
+
